a65e87fa07dc81fb2c4085e3982a5254791b5022
SequoiaQ <taleofnoreturn@gmail.com>
2021-08-27 14:54:51 +0000

    minor changes

--- resources/views/docflows/list.blade.php
+++ resources/views/docflows/list.blade.php
@@ -1,81 +1,83 @@
 <x-layout>
-<table id="table_id" class="dataTable" >
-    <thead>
-        <tr>
-            <th>ID</th> 
-            <th>Docflow-id</th>
-            <th>Docflow-state</th>
-            <th>Content-id</th>
-            <th>Options</th>
-        </tr>
-    </thead>
-    <tbody>
-      @foreach ($docflows as $row)
-          <tr>
-          <td>{{$row['id']}}</td>
-          <td>{{$row['docflow_id']}}</td>
-          <td>{{$row['docflow_state']}}</td>
-          <td>{{$row['content_id']}}</td>
-          <td><a onClick="refreshState('{{$row['docflow_id']}}')" class="btn btn-info">Обновить данные</a></td>
-        </tr>
-      @endforeach
-</tbody>
-    </thead>
-</table>
+    <table id="table_id" class="dataTable">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Docflow-id</th>
+                <th>Docflow-state</th>
+                <th>Content-id</th>
+                <th>Options</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($docflows as $row)
+                <tr>
+                    <td>{{ $row['id'] }}</td>
+                    <td>{{ $row['docflow_id'] }}</td>
+                    <td>{{ $row['docflow_state'] }}</td>
+                    <td>{{ $row['content_id'] }}</td>
+                    <td><a onClick="refreshState('{{ $row['docflow_id'] }}')" class="btn btn-info">Обновить данные</a>
+                    <td><a onClick="deleteDocflowId('{{ $row['docflow_id'] }}')" class="btn btn-danger">Удалить</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+        </thead>
+    </table>
 
-<a href="/doclflowCreate" class="btn btn-success">Создать новый документооборот</a>
+    <a href="/doclflowCreate" class="btn btn-success">Создать новый документооборот</a>
 
-{{--Документ по определенному docflowId --}}
-<a href="docflowid" class="btn btn-success">Получить документ</a>
+    {{-- Документ по определенному docflowId --}}
+    <a href="docflowid" class="btn btn-success">Получить документ</a>
 
 
-<x-scripts></x-scripts>
+    <x-scripts></x-scripts>
 
-<script>
-window.onload = function () {
-fetchingTable();
-}
-
-function refreshState (docflowId) {
-    $.ajax({
-        processData: false,
-        contentType: false,
-        url: `/refreshDocflow/${docflowId}`,
-        type: "GET",
-        dataType: "json",
-        success: function (response) {
-            alert(response.data);
+    <script>
+        window.onload = function() {
             fetchingTable();
         }
-    })
-}
 
-function deleteDocflowId (docflowId) {
-    if (!confirm('Вы действительно хотите удалить файл?')) {
-        return;
-    }
-    $.ajax({
-        url:`/docflows/${docflowId}`,
-        type:"DELETE",
-        success: (response) => {
-          alert(response)
-          fetchingTable()
+        function refreshState(docflowId) {
+            $.ajax({
+                processData: false,
+                contentType: false,
+                url: `/refreshDocflow/${docflowId}`,
+                type: "GET",
+                dataType: "json",
+                success: function(response) {
+                    alert(response.data);
+                    fetchingTable();
+                }
+            })
         }
-    })
-}
 
-function fetchingTable() {
-    $.ajax({
-        processData: false,
-        contentType: false,
-        url: "/docflows",
-        type: "GET",
-        dataType: "json",
-        success: function (response) {
+        function deleteDocflowId(docflowId) {
+            if (!confirm('Вы действительно хотите удалить файл?')) {
+                return;
+            }
+            $.ajax({
+                url: `/docflows/${docflowId}`,
+                type: "DELETE",
+                success: (response) => {
+                    alert(response)
+                    fetchingTable()
+                }
+            })
+        }
 
-            $('tbody').html('')
-            $.each(response.attachment, function (key, item) {
-                $('tbody').append(`
+        function fetchingTable() {
+            $.ajax({
+                processData: false,
+                contentType: false,
+                url: "/docflows",
+                type: "GET",
+                dataType: "json",
+                success: function(response) {
+
+                    $('tbody').html('')
+                    $.each(response.attachment, function(key, item) {
+                        $('tbody').append(`
                   <tr>
                   <td>${item['id']}</td>
                     <td>${$item['docflow_id']}</td>
@@ -84,11 +86,9 @@ function fetchingTable() {
                     <td><a onClick="deleteDocflowId('${$item['docflow_id']}')" class="btn btn-danger" >Удалить</a></td>
                     <a href="('${item['docflowId']}')" class="btn btn-success">Получить файл</a>
                   </tr>`);
+                    })
+                }
             })
         }
-    })
-} 
-
-
-</script>
+    </script>
 </x-layout>
